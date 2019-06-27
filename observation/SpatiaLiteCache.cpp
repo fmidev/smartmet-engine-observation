1f86a44b993392746c4bcb3b394970a3a2a70928
Mika Heiskanen <mika.heiskanen@fmi.fi>
2019-06-27 12:37:33 +0300

    Fixed code to check if observations_fmi or observations_fmi_extaws data is in the SpatiaLite cache

--- observation/SpatiaLiteCache.cpp
+++ observation/SpatiaLiteCache.cpp
@@ -542,12 +542,14 @@ bool SpatiaLiteCache::dataAvailableInCache(const Settings &settings) const
     // If stationtype is cached and if we have requested time interval in
     // SpatiaLite, get all data
     // from there
-    if (settings.stationtype == "opendata" || settings.stationtype == "fmi" ||
-        settings.stationtype == "opendata_mareograph" || settings.stationtype == "opendata_buoy" ||
-        settings.stationtype == "research" || settings.stationtype == "syke")
+    if (settings.stationtype == "observations_fmi" || settings.stationtype == "opendata" ||
+        settings.stationtype == "fmi" || settings.stationtype == "opendata_mareograph" ||
+        settings.stationtype == "opendata_buoy" || settings.stationtype == "research" ||
+        settings.stationtype == "syke")
       return timeIntervalIsCached(settings.starttime, settings.endtime);
 
-    else if (settings.stationtype == "road" || settings.stationtype == "foreign")
+    else if (settings.stationtype == "road" || settings.stationtype == "foreign" ||
+                 settings.stationtype = "observations_fmi_extaws")
       return timeIntervalWeatherDataQCIsCached(settings.starttime, settings.endtime);
 
     else if (settings.stationtype == "flash")
