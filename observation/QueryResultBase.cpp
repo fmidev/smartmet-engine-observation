d5b7786925f0e509d0c8c24b667f87cd2e9fdca3
Mika Heiskanen <mika.heiskanen@fmi.fi>
2020-12-07 09:29:30 +0200

    Added destructor into cpp file to avoid code duplication

---
+++ observation/QueryResultBase.cpp
@@ -0,0 +1,13 @@
+#include "QueryResultBase.h"
+
+namespace SmartMet
+{
+namespace Engine
+{
+namespace Observation
+{
+QueryResultBase::~QueryResultBase() = default;
+
+}  // namespace Observation
+}  // namespace Engine
+}  // namespace SmartMet
