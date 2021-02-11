16e04236a211705012cb3bec3ef12827997f7afa
Anssi Reponen <anssi.reponen@fmi.fi>
2021-02-11 13:28:08 +0200

    Fixed special parameter handling in spatialite queries.

--- observation/CommonDatabaseFunctions.cpp
+++ observation/CommonDatabaseFunctions.cpp
@@ -510,15 +510,11 @@ Spine::TimeSeries::TimeSeriesVectorPtr CommonDatabaseFunctions::buildTimeseries(
 	  
 	  std::set<int> not_null_columns;
 
-          std::set<std::string> not_null_params{
-            "fmisid", "stationname", "stationlongitude", "stationlatitude", "stationlon", "stationlat"};
-
-          for (const auto &p : not_null_params)
-          {
-            auto it = qmap.specialPositions.find(p);
-            if (it != qmap.specialPositions.end())
-              not_null_columns.insert(it->second);
-          }
+	  for(const auto& item : qmap.specialPositions)
+		{
+		  if(Spine::is_special_parameter(item.first))
+			not_null_columns.insert(item.second);			
+		}
 	  
 	  Spine::TimeSeries::TimeSeriesVectorPtr resultVector = initializeResultVector(settings.parameters);
 	  for(const auto& item : station_data)
--- smartmet-engine-observation.spec
+++ smartmet-engine-observation.spec
@@ -3,7 +3,7 @@
 %define SPECNAME smartmet-engine-%{DIRNAME}
 Summary: SmartMet Observation Engine
 Name: %{SPECNAME}
-Version: 21.2.10
+Version: 21.2.11
 Release: 1%{?dist}.fmi
 License: FMI
 Group: SmartMet/Engines
@@ -108,6 +108,10 @@ rm -rf $RPM_BUILD_ROOT
 %{_includedir}/smartmet/engines/%{DIRNAME}
 
 %changelog
+* Thu Feb 11 2021 Anssi Reponen <anssi.reponen@fmi.fi> - 21.2.11-1.fmi
+- Fixed special parameter handling in spatialite queries. No more null values 
+for special parameters when timestep is not found in data.
+
 * Wed Feb 10 2021 Mika Heiskanen <mika.heiskanen@fmi.fi> - 21.2.10-1.fmi
 - Fixed stationlongitude and -latitude to work in direct spatialite queries
 
