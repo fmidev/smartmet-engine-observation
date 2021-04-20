592a9e18708ab88ac790c0abb264cf03c2c576cd
Mika Heiskanen <mika.heiskanen@fmi.fi>
2021-04-20 14:49:31 +0300

    Brainstorm 2039 (#55)
    * Added support for station info request (SMARTMET-2039)
    
    Co-authored-by: Anssi Reponen <anssi.reponen@fmi.fi>

---
+++ observation/StationOptions.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace SmartMet
+{
+namespace Engine
+{
+namespace Observation
+{
+struct StationOptions
+{
+  std::set<int> fmisid;
+  std::set<int> lpnn;
+  std::set<int> wmo;
+  std::set<int> rwsid;
+  std::string type;
+  std::string name;  
+  std::string iso2;
+  std::string region;
+  std::string timeformat;
+  boost::posix_time::ptime start_time;
+  boost::posix_time::ptime end_time;
+  boost::optional<Spine::BoundingBox> bbox;
+};
+
+}  // namespace Observation
+}  // namespace Engine
+}  // namespace SmartMet
