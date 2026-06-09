3c598c25452689b102ac78a906db5f10c2941a3f
Andris Pavēnis <andris.pavenis@fmi.fi>
2026-06-09 13:35:52 +0300

    AsDouble.h: no need to support old libpqxx. Also improve error reporting

--- observation/AsDouble.h
+++ observation/AsDouble.h
@@ -9,28 +9,40 @@ namespace Engine
 {
 namespace Observation
 {
-// PQXX version 5 uses stringstreams which take locks on the global locale with gcc implementation.
-//
-// The field type is templated so the same helpers accept both pqxx::field (libpqxx <= 7) and the
-// pqxx::field_ref proxy returned when iterating rows in libpqxx 8.
 template <typename FieldType>
 inline double as_double(const FieldType& obj)
+try
 {
-#if PQXX_VERSION_MAJOR > 5
   return obj.template as<double>();
-#else
-  return Fmi::stod(obj.template as<std::string>());
-#endif
+}
+catch (...)
+{
+  auto err = Fmi::Exception::Trace(BCP, "Failed to convert field to double: ");
+  if (obj.is_null())
+    err.addDetail("Field is null");
+  else
+  {
+    err.addDetail("Field value: '" + obj.template as<std::string>() + "'");
+  }
+  throw err;
 }
 
 template <typename FieldType>
 inline int as_int(const FieldType& obj)
+try
 {
-#if PQXX_VERSION_MAJOR > 5
   return obj.template as<int>();
-#else
-  return Fmi::stoi(obj.template as<std::string>());
-#endif
+}
+catch (...)
+{
+  auto err = Fmi::Exception::Trace(BCP, "Failed to convert field to int: ");
+  if (obj.is_null())
+    err.addDetail("Field is null");
+  else
+  {
+    err.addDetail("Field value: '" + obj.template as<std::string>() + "'");
+  }
+  throw err;
 }
 
 }  // namespace Observation
